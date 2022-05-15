12a38aed2dceb345792457250006ba1bce5a4b98
Faez-B <faezbhatti@gmail.com>
2022-05-15 20:27:12 +0200

    Fin de l'exercice 8

--- README.md
+++ README.md
@@ -98,3 +98,60 @@ app.get("/hey", (req,res) => {
     res.send("Hey");
 })
 </code>
+
+## Exercice 8
+
+Tester dans le terminal : 
+<code>http :8000</code>
+<code>http PUT :8000 number=2</code>
+<code>http DELETE :8000</code>
+
+<code>
+
+function modifyRequestObject(req, res, next) {
+	req.nouveau = { nouveauTruc: "je nexistais pas avant"};
+	next();
+}
+
+function modifyResponseObject(req, res, next) {
+	res.locals.blabla = "je nexistais pas avant";
+	next();
+}
+
+function blockDeleteRequest(req, res,next){;
+	if (req.method === "DELETE") {
+		throw new Error('NO DELETE PLEASE');
+	}
+	else next();
+}
+
+function blockPutRequest(req, res,next){
+	if (req.method === "PUT") {
+		res.status(400).send('NO DELETE PLEASE');
+        // Pas besoin de next() ici
+        // On envoie le code 400 avec status() avec res
+        // Et on fait un send() de message (!! ON NE FAIT PAS UN SEND DU STATUS CODE)
+        // Le send() interromps la chaine de middleware
+        // Donc à faire en dernier.
+	}
+	else {next()}
+}
+
+
+app.use(blockDeleteRequest);
+app.use(modifyRequestObject);
+app.use(modifyResponseObject);
+app.use(blockPutRequest);
+
+app.get('/', (req, res) => {
+	console.log(req.nouveau)
+	console.log(res.locals)
+	res.send('Hello World');
+})
+
+app.put('/', (req, res) => {
+    // jamais exécuté.
+    console.log("I AM IN"); 
+	res.send('Hello');
+})
+</code>
--- app.js
+++ app.js
@@ -4,27 +4,51 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT;
 
-function middleware1(req, res, next) {
-    console.log("middleware 1", req.method, req.url);
-    next(); 
-    // Il faut appeler next() dans les middleware qu'on créer pour passer au middleware suivant
+function modifyRequestObject(req, res, next) {
+	req.nouveau = { nouveauTruc: "je nexistais pas avant"};
+	next();
 }
 
-function middleware2(req, res, next) {
-    console.log("middleware 2", req.method, req.url);
-    next(); 
+function modifyResponseObject(req, res, next) {
+	res.locals.blabla = "je nexistais pas avant";
+	next();
 }
 
-app.use(middleware1);
-app.use(middleware2);
-app.use(middleware1);
+function blockDeleteRequest(req, res,next){;
+	if (req.method === "DELETE") {
+		throw new Error('NO DELETE PLEASE');
+	}
+	else next();
+}
+
+function blockPutRequest(req, res,next){
+	if (req.method === "PUT") {
+		res.status(400).send('NO DELETE PLEASE');
+        // Pas besoin de next() ici
+        // On envoie le code 400 avec status() avec res
+        // Et on fait un send() de message (!! ON NE FAIT PAS UN SEND DU STATUS CODE)
+        // Le send() interromps la chaine de middleware
+        // Donc à faire en dernier.
+	}
+	else {next()}
+}
+
+
+app.use(blockDeleteRequest);
+app.use(modifyRequestObject);
+app.use(modifyResponseObject);
+app.use(blockPutRequest);
 
 app.get('/', (req, res) => {
-    res.send('Hello world');
+	console.log(req.nouveau)
+	console.log(res.locals)
+	res.send('Hello World');
 })
 
-app.get("/hey", (req,res) => {
-    res.send("Hey");
+app.put('/', (req, res) => {
+    // jamais exécuté.
+    console.log("I AM IN"); 
+	res.send('Hello');
 })
 
 app.listen(port, () => {
