4bd4b34a39ed676051e2b34bf4974be7b41da813
Faez-B <faezbhatti@gmail.com>
2022-05-15 15:37:19 +0200

    Sans le middleware express.json() On ne peut pas parser les arguments passer dans la requête

--- app.js
+++ app.js
@@ -2,15 +2,12 @@ require("dotenv").config();
 const express = require('express');
 const path = require('path');
 const app = express();
-
 const port = process.env.PORT;
 
-app.use("/public", express.static(path.join(__dirname, 'public')));
-
-app.get("/" , () => {
-    // res.send(path.join(__dirname, 'public'));
-
-});
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
