ca4c4f8a11c95cf21f87f7605553e6d56c70c78f
Faez-B <faezbhatti@gmail.com>
2022-05-15 20:09:21 +0200

    Fin de l'exercice 7

--- README.md
+++ README.md
@@ -70,4 +70,31 @@ app.get("/:arg1/test/:arg2", (req, res) => {
 })
 </code>
 
+## Exercice 7
+Utiliser des middleware persos
+Test dans le terminal : <code>curl http://localhost:3000</code>
 
+<code>
+function middleware1(req, res, next) {
+    console.log("middleware 1", req.method, req.url);
+    next(); 
+    // Il faut appeler next() dans les middleware qu'on créer pour passer au middleware suivant
+}
+
+function middleware2(req, res, next) {
+    console.log("middleware 2", req.method, req.url);
+    next(); 
+}
+
+app.use(middleware1);
+app.use(middleware2);
+app.use(middleware1);
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+})
+
+app.get("/hey", (req,res) => {
+    res.send("Hey");
+})
+</code>
--- app.js
+++ app.js
@@ -4,9 +4,27 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-    console.log(req.query);
-    res.send(req.query);
+function middleware1(req, res, next) {
+    console.log("middleware 1", req.method, req.url);
+    next(); 
+    // Il faut appeler next() dans les middleware qu'on créer pour passer au middleware suivant
+}
+
+function middleware2(req, res, next) {
+    console.log("middleware 2", req.method, req.url);
+    next(); 
+}
+
+app.use(middleware1);
+app.use(middleware2);
+app.use(middleware1);
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+})
+
+app.get("/hey", (req,res) => {
+    res.send("Hey");
 })
 
 app.listen(port, () => {
