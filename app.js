86882ee61235ad80e153c89421df7776d0c2bfbe
Faez-B <faezbhatti@gmail.com>
2022-05-15 15:32:28 +0200

    End of exercise n°2.

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const express = require('express');
 require("dotenv").config();
+const express = require('express');
+const path = require('path');
 const app = express();
 
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.send("Hello world !"); 
-    // res.send() envoie le message et effectue la commande res.end() pour nous
-})
+app.use("/public", express.static(path.join(__dirname, 'public')));
+
+app.get("/" , () => {
+    // res.send(path.join(__dirname, 'public'));
+
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
