4d862b6a1f80f0b924ad3bc2d49cf04bd06f2567
Faez-B <faezbhatti@gmail.com>
2022-05-15 15:41:54 +0200

    End of exercise n°3

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.json());
+
 app.post("/", (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+    console.log(req.body); // affichage dans la console // dans le terminal => là où on lancer le serveur
+    res.send(req.body); // affichage là où on a passer la requête http
 })
 
 app.listen(port, () => {
