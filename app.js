a6d1ee88493be4f9de115d02b038ef521f08758a
Faez-B <faezbhatti@gmail.com>
2022-05-15 23:02:48 +0200

    Crash à cause de async

--- app.js
+++ app.js
@@ -4,24 +4,14 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT;
 
-function errorMiddleware(err, req, res, next) {
-	res.status(500).json({erreur: err.message});
-}
-
-app.get('/', (req, res, next) => {
-	throw new Error('Ceci est un bug dans la route /');
-	res.send('Pas executé');
+app.get('/', async (req, res) => {
+	throw new Error("Je suis un ASYNC bug")
+	res.send('JE NE SERAI JAMAIS EXECUTE');
 })
 
-app.get('/route2', (req, res, next) => {
-	throw new Error('Ceci est un bug dans /route 2');
-	res.send('Pas executé');
+app.use((err, req, res, next) => {
+	res.status(500).json({erreur: err.message})
 })
-
-// TOUJOURS UTILISER LE MIDDLEWARE ERREUR À LA FIN
-// APRÈS LES DÉFINITIONS ET LES AUTRES MIDDLEWARE
-app.use(errorMiddleware);
-
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
