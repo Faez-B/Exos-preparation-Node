9a0e17de7bf62c3f1dac7380519c9b33d117fe31
Faez-B <faezbhatti@gmail.com>
2022-05-17 13:37:19 +0200

    Récupération de tous les comptes existants dans la collection. Ajout d'un nouveau compte (sans hachage)

--- app.js
+++ app.js
@@ -4,45 +4,50 @@ const express = require('express');
 const path = require('path');
 const Joi = require("joi");
 const db = require("./db");
+const Collection = require("./Collection");
 const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT;
 
-const token = jwt.sign({userId: 12}, process.env.SECRET_JWT);
-console.log("token JWT", token);
+const Comptes = new Collection('Comptes');
 
-const decoded = jwt.verify(token, process.env.SECRET_JWT);
-console.log("token JWT décodé", decoded);
+app.use(express.json());
 
-// verify avec une chaine incorrecte / 
-// chaine différente que celle qu'on a utilisé pour faire le sign
+app.get('/comptes/', (req, res) => {
+	res.json(Comptes.getAll());
+})
 
-// Match véritable token contre faux string
-try {
-	const notdecoded = jwt.verify(token, "PAS_LE_BON_SECRET");
-}catch(exc){
-	console.log("échec de jwt.verify : PAS LA BONNE SIGNATURE");
+app.post('/enregistrer/', (req, res) => {
+	const data = req.body;
+
+	const schema = Joi.object({
+		name: Joi.string().min(2).max(255).required(),
+		password: Joi.string().min(6).max(255).required()
+	})
+
+	const { value, error } = schema.validate(data);
+
+	if (error) {
+		res.status(400).send({ erreur : error.details[0].message })
+		// Si erreur alors on s'arrête ici
+	}
+	else {
+		Comptes.insertOne(value);
+
+		// 201 : PUT, POST
+		res.status(201).json({
+			name: value.name
+		})
+	}
+})
+
+
+if (process.env.NODE_ENV !== "test") {
+
+	app.listen(port, () => {
+		console.log(`Listening on port ${port}...`);
+	});
+
 }
 
-
-// Match faux token contre vrai string
-
-const token2 = jwt.sign({userId: 13}, process.env.SECRET_JWT);
-
-const header = token.split('.')[0];
-const payload = token.split('.')[1];
-const signature = token2.split('.')[2];
-
-const fakeToken = header +"."+ payload +"."+ signature
-console.log("FAKE TOKEN : ", fakeToken);
-
-
-try {
-  const failedVerification = jwt.verify(fakeToken, process.env.SECRET_JWT);
-}catch(exc){
-  console.log('echec de la vérification du Faux Token : PAS LA BONNE SIGNATURE');
-}
-
-app.listen(port, () => {
-    console.log(`Listening on port ${port}...`);
-});
+module.exports = { app, Comptes };
